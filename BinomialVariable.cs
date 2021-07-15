73cd5555dcfd4ac92680aef9d50c33b7b9350cd6
Ofsouzap <ofsouzap@gmail.com>
2021-07-15 15:20:27 +0100

    Implemented binomially-distributed random variable

--- BinomialVariable.cs
+++ BinomialVariable.cs
@@ -19,8 +19,29 @@ namespace DistributedMonteCarloSimulation.RandomVariables
         public override double Evaluate(Dictionary<string, double> variableMappings, Random random)
         {
 
-            //TODO
-            throw new NotImplementedException();
+            double probabilityTotal = 0;
+            double r = random.NextDouble();
+
+            for (uint i = 0; i < trials; i++)
+            {
+
+                probabilityTotal += ProbabilityOfResultValue(i);
+
+                if (r <= probabilityTotal)
+                    return i;
+
+            }
+
+            return trials;
+
+        }
+
+        public double ProbabilityOfResultValue(uint value)
+        {
+
+            return Maths.Combinations(trials, value)
+                * Math.Pow(successProbability, value)
+                * Math.Pow(1 - successProbability, trials - value);
 
         }
 
---
+++ Maths.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace DistributedMonteCarloSimulation
+{
+    public static class Maths
+    {
+
+        public static uint Combinations(uint n, uint r)
+        {
+
+            return (Factorial(n)) / (Factorial(r) * Factorial(n - r));
+
+        }
+
+        public static uint Factorial(uint x)
+        {
+
+            if (x == 0)
+                return 1;
+            else if (x == 1)
+                return 1;
+            else
+                return x * Factorial(x - 1);
+
+        }
+
+    }
+}
